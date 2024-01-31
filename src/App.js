7b7d84ebd60418952d830986123f92c2c0dabbcf
gudimetla srikanth <gudimetlasrikanth1@gmail.com>
2024-01-31 20:46:16 +0530

    committed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   const [resData, setResData] = useState([])
   useEffect(() => {
     fetch('https://api.tvmaze.com/search/shows?q=all').then(data => data.json()).then(data =>
-      setResData(data))
+      setResData(data)).catch(err => console.log(err))
   }, [])
   if (resData.length === 0) return <h1>Loading...</h1>
   return (
